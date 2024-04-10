761894c8be01c4029663e4c58825bba5fe1f7eba
cojovi <31863818+cojovi@users.noreply.github.com>
2024-04-10 04:11:45 -0500

    first round

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -10,7 +10,7 @@ const Collaboration = () => {
       <div className="container lg:flex">
         <div className="max-w-[25rem]">
           <h2 className="h2 mb-4 md:mb-8">
-            AI Chat App for seamless collaboration
+            Custom AI Web Apps for seamless collaboration
           </h2>
           <ul className="max-w-[22rem] mb-10 md:mb-14">
             {collabContent.map((item) => (
