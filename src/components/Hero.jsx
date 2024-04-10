294fb6f4f63f1fdb2a762d79ef4e96ffd6f45c64
cojovi <31863818+cojovi@users.noreply.github.com>
2024-04-10 03:52:39 -0500

    2
    2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,9 +23,9 @@ const Hero = () => {
       <div className="container realtive" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
+            Custom Website Development and&nbsp;&nbsp;Comprehensive Online Solutions with {` `}
             <span className="inline-block relative">
-              Brain<span className="text-purple-500">Master</span>{" "}
+              Cojovi<span className="text-purple-500">Webwrox</span>{" "}
               <img
                 src={curve}
                 className="absolute top-full left-0 w-full xl:-mt-2"
@@ -36,11 +36,11 @@ const Hero = () => {
             </span>
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-            Unleash the power of AI within Brainmaster. Upgrade your
-            productivity with Brainmaster, the open AI chat app.
+            Your vision, our digital craftsmanship. Upgrade your
+            productivity with SEO Optimization, enhanced digital presence, and our custom ai integrations to automate workflows.
           </p>
           <Button href="/pricing" white>
-            Get Started
+            Get More Info
           </Button>
         </div>
         <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
