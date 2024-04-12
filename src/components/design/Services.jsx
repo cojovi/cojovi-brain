fb87773356c21a1109cf85f652a20772c73e6947
cojovi <31863818+cojovi@users.noreply.github.com>
2024-04-11 20:45:05 -0500

    3
    3

--- src/components/design/Services.jsx
+++ src/components/design/Services.jsx
@@ -18,7 +18,7 @@ export const Gradient = () => {
 export const PhotoChatMessage = () => {
   return (
     <div className="absolute top-8 right-8 max-w-[17.5rem] py-6 px-8 bg-black rounded-t-xl rounded-bl-xl font-code text-base lg:top-16 lg:right-[8.75rem] lg:max-w-[17.5rem]">
-      You have <span className="text-purple-500 font-bold">Twelve</span>unread emails from online queries!
+      You have <span className="text-purple-500 font-bold">Twelve </span>unread emails from online queries!
       <ChatBubbleWing className="absolute left-full bottom-0" />
     </div>
   );
