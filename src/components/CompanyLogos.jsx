24c5bca5833d406c463b6a39395f91fbec3756e5
cojovi <31863818+cojovi@users.noreply.github.com>
2024-04-11 20:06:50 -0500

    small update

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -4,7 +4,7 @@ const CompanyLogos = ({ className }) => {
   return (
     <div className={className}>
       <h2 className="tagline mb-6 text-center text-n-1/50">
-        Helping people create beautiful content at
+        Helping companies upscale their online presence 
       </h2>
       <ul className="flex">
         {companyLogos.map((logo, index) => (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
             src={brainmaster}
             width={196}
             height={40}
-            alt="Brainmaster logo"
+            alt="cojovi webworx logo"
           />
         </a>
         <nav
